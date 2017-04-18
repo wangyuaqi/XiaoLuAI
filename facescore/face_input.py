b37ad967bc8f5f8650e3edcc9e1281a2b76cd566
EclipseXuLu <249048056@qq.com>
2017-04-18 15:56:14 +0800

    build cnn model for face recognition

---
+++ facescore/face_input.py
@@ -0,0 +1,114 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import os
+
+IMAGE_SIZE = 144
+NUM_CLASSED = 5
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
+
+def read_face(filename_queue):
+    class Face():
+        pass
+
+    face = Face()
+    label_bytes = 1
+    face.height = 144
+    face.width = 144
+    face.depth = 3
+    image_bytes = face.height * face.width * face.depth
+    record_bytes = image_bytes + label_bytes
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+    face.key, value = reader.read(filename_queue)
+
+    record_bytes = tf.decode_raw(value, tf.uint8)
+    face.label = tf.cast(tf.strided_slice(record_bytes, [0], tf.int32))
+
+    depth_major = tf.reshape(record_bytes, [label_bytes], [label_bytes + image_bytes])
+    face.uint8image = tf.transpose(depth_major, [1, 2, 0])
+
+    return face
+
+
+def _generate_image_and_label_batch(image, label, min_queue_examples, batch_size, shuffle):
+    num_process_threads = 16
+    if shuffle:
+        images, label_batch = tf.train.shuffle_batch([image, label], batch_size=batch_size,
+                                                     num_threads=num_process_threads,
+                                                     capacity=min_queue_examples + 3 * batch_size,
+                                                     min_after_dequeue=min_queue_examples)
+    else:
+        images, label_batch = tf.train.batch([image, label], batch_size=batch_size, num_threads=num_process_threads,
+                                             capacity=min_queue_examples + 3 * batch_size)
+    tf.summary.image('image', images)
+
+    return images, tf.reshape(label_batch, [batch_size])
+
+
+def distorted_inputs(data_dir, batch_size):
+    filenames = [os.path.join(data_dir, 'training_set.bin')]
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Error to find file' + f)
+    filename_queue = tf.train.string_input_producer(filenames)
+    read_input = read_face(filename_queue)
+    reshaped_image = tf.cast(read_input.uint8image, tf.float32)
+
+    height = IMAGE_SIZE
+    width = IMAGE_SIZE
+
+    # subtract off the mean and divide by the variance of the pixels
+    float_image = tf.image.per_image_standardization(reshaped_image)
+
+    # set the shape of tensors
+    float_image.set_shape([height, width, 3])
+    read_input.label.set_shape([1])
+
+    # ensure that the random shuffling has a good mixing properties
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
+    print('Filling queue with %d CIFAR images before starting to train. '
+          'This will take a few minutes.' % min_queue_examples)
+
+    return _generate_image_and_label_batch(float_image, read_input.label, min_queue_examples, batch_size, shuffle=True)
+
+
+def inputs(eval_data, data_dir, batch_size):
+    if not eval_data:
+        filenames = [os.path.join(data_dir, 'training_set.bin')]
+        num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+    else:
+        filenames = [os.path.join(data_dir, 'test_set.bin')]
+        num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file ' + f)
+
+    filename_queue = tf.train.string_input_producer(filenames)
+    read_input = read_face(filename_queue)
+    reshaped_image = tf.cast(read_input.uint8image, tf.float32)
+
+    height = IMAGE_SIZE
+    width = IMAGE_SIZE
+
+    # Subtract off the mean and divide by the variance of the pixels.
+    float_image = tf.image.per_image_standardization(reshaped_image)
+
+    # Set the shapes of tensors.
+    float_image.set_shape([height, width, 3])
+    read_input.label.set_shape([1])
+
+    # Ensure that the random shuffling has good mixing properties.
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(num_examples_per_epoch *
+                             min_fraction_of_examples_in_queue)
+
+    # Generate a batch of images and labels by building up a queue of examples.
+    return _generate_image_and_label_batch(float_image, read_input.label,
+                                           min_queue_examples, batch_size,
+                                           shuffle=False)
---
+++ facescore/face_multi_gpu_train.py
@@ -0,0 +1,158 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import os.path
+import re
+import time
+
+import numpy as np
+import tensorflow as tf
+from facescore import face
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('train_dir', '/tmp/face',
+                           """Directory where to write event logs """
+                           """and checkpoint.""")
+tf.app.flags.DEFINE_integer('max_steps', 1000000,
+                            """Number of batches to run.""")
+tf.app.flags.DEFINE_integer('num_gpus', 2,
+                            """How many GPUs to use.""")
+tf.app.flags.DEFINE_boolean('log_device_placement', False,
+                            """Whether to log device placement.""")
+
+
+def tower_loss(scope):
+    images, labels = face.distorted_inputs()
+    logits = face.inference(images)
+
+    _ = face.loss(logits, labels)
+    losses = tf.get_collection('losses', scope)
+    total_loss = tf.add_n(losses, name='total_loss')
+    for i in losses + [total_loss]:
+        loss_name = re.sub('%s_[0-9]' % face.TOWER_NAME, '', i.op.name)
+        tf.summary.scalar(loss_name, i)
+
+    return total_loss
+
+
+def average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        grads = []
+        for g, _ in grad_and_vars:
+            expanded_g = tf.expand_dims(g, 0)
+            grads.append(expanded_g)
+
+        grad = tf.concat(axis=0, values=grad_and_vars)
+        grad = tf.reduce_mean(grad, 0)
+
+        v = grad_and_vars[0][1]
+        grad_and_var = (grad, v)
+        average_grads.append(grad_and_var)
+
+    return average_grads
+
+
+def train():
+    with tf.Graph().as_default(), tf.device('/cpu:0'):
+        global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+        num_batches_per_epoch = (face.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size)
+        decay_steps = int(num_batches_per_epoch * face.NUM_EPOCHS_PER_DECAY)
+
+        lr = tf.train.exponential_decay(face.INITIAL_LEARNING_RATE, global_step, decay_steps,
+                                        face.LEARNING_RATE_DECAY_FACTOR)
+        opt = tf.train.GradientDescentOptimizer(lr)
+
+        tower_grads = []
+        with tf.variable_scope(tf.get_variable_scope()):
+            for i in range(FLAGS.num_gpus):
+                with tf.device('/gpu:%d' % i):
+                    with tf.name_scope('%s_%d' % (face.TOWER_NAME, i)) as scope:
+                        loss = tower_loss((scope))
+                        tf.get_variable_scope().reuse_variables()
+                        summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
+                        grads = opt.compute_gradients(loss)
+                        tower_grads.append(grads)
+        grads = average_gradients(tower_grads)
+        summaries.append(tf.summary.scalar('learning_rate', lr))
+        for grad, var in grads:
+            if grad is not None:
+                summaries.append(tf.summary.histogram(var.op.name + '/gradients', grad))
+
+                # Apply the gradients to adjust the shared variables.
+        apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+        # Add histograms for trainable variables.
+        for var in tf.trainable_variables():
+            summaries.append(tf.summary.histogram(var.op.name, var))
+
+        # Track the moving averages of all trainable variables.
+        variable_averages = tf.train.ExponentialMovingAverage(
+            face.MOVING_AVERAGE_DECAY, global_step)
+        variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+        # Group all updates to into a single train op.
+        train_op = tf.group(apply_gradient_op, variables_averages_op)
+
+        # Create a saver.
+        saver = tf.train.Saver(tf.global_variables())
+
+        # Build the summary operation from the last tower summaries.
+        summary_op = tf.summary.merge(summaries)
+
+        # Build an initialization operation to run below.
+        init = tf.global_variables_initializer()
+
+        # Start running operations on the Graph. allow_soft_placement must be set to
+        # True to build towers on GPU, as some of the ops do not have GPU
+        # implementations.
+        sess = tf.Session(config=tf.ConfigProto(
+            allow_soft_placement=True,
+            log_device_placement=FLAGS.log_device_placement))
+        sess.run(init)
+
+        # Start the queue runners.
+        tf.train.start_queue_runners(sess=sess)
+
+        summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
+
+        for step in range(FLAGS.max_steps):
+            start_time = time.time()
+            _, loss_value = sess.run([train_op, loss])
+            duration = time.time() - start_time
+
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 10 == 0:
+                num_examples_per_step = FLAGS.batch_size * FLAGS.num_gpus
+                examples_per_sec = num_examples_per_step / duration
+                sec_per_batch = duration / FLAGS.num_gpus
+
+                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                              'sec/batch)')
+                print(format_str % (datetime.now(), step, loss_value,
+                                    examples_per_sec, sec_per_batch))
+
+            if step % 100 == 0:
+                summary_str = sess.run(summary_op)
+                summary_writer.add_summary(summary_str, step)
+
+            # Save the model checkpoint periodically.
+            if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+    face.maybe_download_and_extract()
+    if tf.gfile.Exists(FLAGS.train_dir):
+        tf.gfile.DeleteRecursively(FLAGS.train_dir)
+    tf.gfile.MakeDirs(FLAGS.train_dir)
+    train()
+
+
+if __name__ == '__main__':
+    tf.app.run()

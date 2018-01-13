f3ab3eb5d725c2dc209f9b54842e8012da5b7274
EclipseXuLu <249048056@qq.com>
2018-01-13 16:45:51 +0800

    update warp and padding image handler

--- facescore/scut_fbp_preprocess.py
+++ facescore/scut_fbp_preprocess.py
@@ -1,29 +1,36 @@
 import os
 
+import numpy as np
 import cv2
 
 SCUT_FBP = '/media/lucasx/Document/DataSet/Face/SCUT-FBP/Data_Collection/Data_Collection'
 IMAGE_SIZE = 224
 
 
-def resample_images(dst_dir='/media/lucasx/Document/DataSet/Face/SCUT-FBP/Processed'):
+def padding_images(dst_dir='/media/lucasx/Document/DataSet/Face/SCUT-FBP/Padding'):
     if not os.path.exists(dst_dir) or not os.path.isdir(dst_dir):
         os.makedirs(dst_dir)
 
     for _ in os.listdir(SCUT_FBP):
         image = cv2.imread(os.path.join(SCUT_FBP, _))
         h, w, c = image.shape
+        dst = np.ones([IMAGE_SIZE, IMAGE_SIZE, c], dtype=np.uint8) * 255
         if h >= w:
-            roi = image[0:w, 0:w, :]
+            ratio = h / IMAGE_SIZE
+            roi = cv2.resize(image, (int(w / ratio), IMAGE_SIZE))
+            padding_width = int((IMAGE_SIZE - int(w / ratio)) / 2)
+            dst[:, padding_width:padding_width + int(w / ratio), :] = roi
         else:
-            roi = image[0:h, 0:h, :]
+            ratio = h / IMAGE_SIZE
+            roi = cv2.resize(image, (IMAGE_SIZE, int(h / ratio)))
+            padding_height = int((IMAGE_SIZE - int(h / ratio)) / 2)
+            dst[padding_height:padding_height + int(h / ratio), :, :] = roi
 
-        roi = cv2.resize(roi, (IMAGE_SIZE, IMAGE_SIZE))
-        cv2.imwrite(os.path.join(dst_dir, _), roi)
+        cv2.imwrite(os.path.join(dst_dir, _), dst)
         print('write image %s' % os.path.join(dst_dir, _))
 
 
-def resize_images(dst_dir='/media/lucasx/Document/DataSet/Face/SCUT-FBP/Processed'):
+def warp_images(dst_dir='/media/lucasx/Document/DataSet/Face/SCUT-FBP/Warp'):
     if not os.path.exists(dst_dir) or not os.path.isdir(dst_dir):
         os.makedirs(dst_dir)
 
@@ -35,4 +42,4 @@ def resize_images(dst_dir='/media/lucasx/Document/DataSet/Face/SCUT-FBP/Processe
 
 
 if __name__ == '__main__':
-    resize_images()
+    padding_images()

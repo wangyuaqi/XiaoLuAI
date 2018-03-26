98887a99bbd4c7ef1f028627c1a309dd49455ec2
EclipseXuLu <249048056@qq.com>
2018-03-26 17:08:00 +0800

    update GNet

--- mtcnet/fbp.py
+++ mtcnet/fbp.py
@@ -1,3 +1,6 @@
+import copy
+import time
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -5,6 +8,8 @@ from torchvision import transforms, datasets
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
 
 from mtcnet import data_loader
 from mtcnet.cfg import cfg
@@ -83,20 +88,24 @@ class GNet(nn.Module):
 
     def __init__(self):
         super(GNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+
+        self.gfc1 = nn.Linear(56 * 56 * 256, 4096)
+        self.gfc2 = nn.Linear(4096, 2)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool4(self.conv4(self.conv3(x)))
+        x = F.relu(self.conv5(x))
 
         return x
 
@@ -109,7 +118,79 @@ class GNet(nn.Module):
         return num_features
 
 
-def train_gnet():
+def train_gnet(model, train_loader, test_loader, criterion, optimizer, scheduler, num_epochs=25):
+    """
+    train GNet
+    :param model:
+    :param criterion:
+    :param optimizer:
+    :param scheduler:
+    :param num_epochs:
+    :return:
+    """
+    tik = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    use_gpu = torch.cuda.is_available()
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        # Iterate over data.
+        for data in train_loader:
+            # get the inputs
+            inputs, labels = data
+
+            # wrap them in Variable
+            if use_gpu:
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
+
+            # backward + optimize only if in training phase
+            loss.backward()
+            optimizer.step()
+
+            # statistics
+            running_loss += loss.data[0] * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(train_loader)
+            epoch_acc = running_corrects / len(train_loader)
+
+            print('Training Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    time_elapsed = time.time() - tik
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    return model
+
+
+def train_hmtnet():
     data_transform = transforms.Compose([
         transforms.RandomSizedCrop(224),
         transforms.RandomHorizontalFlip(),
@@ -117,14 +198,13 @@ def train_gnet():
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-    gender_dataset = datasets.ImageFolder(root=cfg['gender_base_dir'],
-                                          transform=data_transform)
-
-    train_loader, test_loader = data_loader.split_train_and_test_with_py_datasets(gender_dataset, cfg['batch_size'],
-                                                                                  0.2, 4, True)
-
-    print()
 
 
 if __name__ == '__main__':
-    train_gnet()
+    gnet = GNet()
+    train_loader, test_loader = data_loader.split_train_and_test_with_py_datasets()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(gnet.parameters(), lr=0.01)
+
+    train_gnet(gnet, train_loader, test_loader, criterion, optimizer, scheduler=None, num_epochs=10)

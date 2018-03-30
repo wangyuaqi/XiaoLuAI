a00524ff2ff575775c2dfef58df455d8f41ed2d4
EclipseXuLu <249048056@qq.com>
2018-03-30 22:36:50 +0800

    update mate_face_detector

--- facescore/mate_face_detector.py
+++ facescore/mate_face_detector.py
@@ -1,19 +1,20 @@
-import sys
+import math
 import os
+import sys
 
-import numpy as np
 import cv2
-from scipy import spatial
+import numpy as np
 from mtcnn.mtcnn import MTCNN
+from scipy import spatial
 
 
-def detect_face(face_img):
+def detect_face(face_img_file):
     """
     detect face with MTCNN and draw face region with OpenCV
     :param face_img:
     :return:
     """
-    img = cv2.imread(face_img)
+    img = cv2.imread(face_img_file)
     detector = MTCNN()
     result = detector.detect_faces(img)
 
@@ -50,12 +51,30 @@ def detect_face(face_img):
             (hog_from_cv(cv2.resize(face2, (128, 128))), LBP_from_cv(cv2.resize(face2, (128, 128)))))
 
         cos_sim = cal_cos_sim(feature1, feature2)
-        print(cos_sim)
+        print('Cosine Similarity = %f' % cos_sim)
+
+        # detect with dlib
+        from facescore import face_beauty_regressor
+        result = face_beauty_regressor.det_landmarks(face_img_file)
+        geo_dis = cal_geo_dis(result[0]['landmarks'], result[1]['landmarks'])
+        print('Geo distance = %f' % geo_dis)
 
 
 def cal_cos_sim(feature1, feature2):
+    """
+    calculate cosine similarity
+    :param feature1:
+    :param feature2:
+    :return:
+    """
     return 1 - spatial.distance.cosine(feature1, feature2)
 
 
+def cal_geo_dis(face_ldmk_list1, face_ldmk_list2):
+    eu_dis = np.linalg.norm(np.array(face_ldmk_list1) - np.array(face_ldmk_list2))
+
+    return 1 / (1 + math.exp(-eu_dis))
+
+
 if __name__ == '__main__':
-    detect_face('E:/jay.jpg')
+    detect_face('./jay.jpg')
